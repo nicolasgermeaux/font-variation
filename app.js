d8d67d8fb19d0fca6d05c8a7093c29a9dfa07f76
Nicolas Germeaux <36262701+nicolasgermeaux@users.noreply.github.com>
2021-10-30 16:07:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let title = document.querySelector('.idkhowtonameit__title') // On récupère l'
 title.style.setProperty('font-variation-settings', "'wght' " + windowSize) // On lui attribue une font-variation
 
 window.onresize = function(event) {
-	windowSize = window.innerWidth // On refresh la variable à chaque resize
+    windowSize = window.innerWidth // On refresh la variable à chaque resize
     console.log(windowSize) // Tu peux voir les changements de tailles
     title.style.setProperty('font-variation-settings', "'wght' " + windowSize/1.6) // On update la font-variation puis on divise sur 1.6 pour que ça soit étalonner sur un écran de 1440px
 }
