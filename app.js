b8cb1ebe244938732cf3801ffe053378af251744
Nicolas Germeaux <nicolas@monastudio.co>
2021-10-30 14:19:47 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let windowSize = window.innerWidth;
+
+let title = document.querySelector('.idkhowtonameit__title') // On récupère l'élément
+title.style.setProperty('font-variation-settings', "'wght' " + windowSize) // On lui attribue une font-variation
+
+window.onresize = function(event) {
+	windowSize = window.innerWidth; // On refresh la variable à chaque resize
+    console.log(windowSize) // Tu peux voir les changements de tailles
+    title.style.setProperty('font-variation-settings', "'wght' " + windowSize) // On update la font-variation
+}
